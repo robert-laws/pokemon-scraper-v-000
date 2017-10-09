69fbddd40822c56980ec74462109a45452b2cb22
Robert Laws <roblaws@me.com>
2017-10-09 18:10:32 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,7 @@
 class Pokemon
 
-  def initialize(id: nil, name: nil, type: nil, db: nil)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+  def initialize(id:, name:, type:, db:)
+    @id, @name, @type, @db = id, name, type, db
   end
 
 end
